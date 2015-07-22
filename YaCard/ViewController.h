54ea79f4154503d662869e1a06af06c1a537f354
Chen <Day@chn-dachen-mac.local>
2015-07-21 20:15:43 -0700

    Initial Commit

---
+++ YaCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YaCard
+//
+//  Created by Chen, Ya on 7/21/15.
+//  Copyright (c) 2015 Chen, Ya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
